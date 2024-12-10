9fef837827f62527367ce5b974358a1caba875fe
abyynw <abhinavachu672@gmail.com>
2024-12-10 14:28:14 +0530

    fourth commits

--- JavaScript/program1.js
+++
@@ -1,5 +0,0 @@
-x = prompt("Enter a String: ");
-console.log(x);
-document.write("<br> Length: ", Length())
-document.write("Uppercase: ",x,toUpperCase());
-document.write("Uppercase: ",x,toLowerCase());
